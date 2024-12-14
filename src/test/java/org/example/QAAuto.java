a155b39dca30e9f0aa5cef08aca9b6d065c4c117
AndrewStak11 <andrejstakanchuk@gmail.com>
2024-12-13 20:54:27 -0800

    Initial commit

---
+++ src/test/java/org/example/QAAuto.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.time.Duration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+    public class QAAuto {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://skillfactory.ru/courses/java");
+            driver.manage().window().maximize();
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            driver.findElement(By.cssSelector("#Type-1-grey > div.card__image-top > div.card__additional-img")).click();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                Thread.sleep(8000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            driver.quit();
+        }
+    }
+}
